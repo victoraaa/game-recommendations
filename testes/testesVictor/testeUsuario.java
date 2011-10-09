699da6a91d1a1574d835d48cf241f3569698ef65
Victor Araujo <victoraaa@gmail.com>
2011-10-09 01:21:52 -0300

    agora sim, com testes

---
+++ testes/testesVictor/testeUsuario.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package testesVictor;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Victor
+ */
+public class testeUsuario {
+
+    public testeUsuario() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+}
